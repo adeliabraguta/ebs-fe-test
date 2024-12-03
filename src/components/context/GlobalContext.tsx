3eeab221387d4aee5c419166a48c6e41f85855af
adeliabraguta <adeliabraguta@gmail.com>
2024-12-03 12:13:55 +0200

    feat: create global context for managing state

---
+++ src/components/context/GlobalContext.tsx
@@ -0,0 +1,20 @@
+import React, {createContext, ReactNode, useState} from "react";
+import {Product} from "../../types/productTypes.ts";
+
+type GlobalContextType = {
+    cartProducts: Product[]
+}
+
+export const GlobalContext = createContext<GlobalContextType | null >(null)
+
+const GlobalProvider: React.FC<{ children: ReactNode }> = ({children}) => {
+    const [cartProducts, setCartProducts] = useState<Product[]>([]);
+
+    return (
+        <GlobalContext.Provider value={{cartProducts}}>
+            {children}
+        </GlobalContext.Provider>
+    );
+};
+
+export default GlobalProvider;
