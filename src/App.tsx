2e9aa6b777a14062aec6990661d533cf492927af
adeliabraguta <adeliabraguta@gmail.com>
2024-12-03 08:28:13 +0200

    feat: add main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Header from "./components/header/Header.tsx";
 import Footer from "./components/footer/Footer.tsx";
+import MainContent from "./components/mainContent/MainContent.tsx";
 
 function App() {
-  return (
-    <div>
-      <Header/>
-        <Footer/>
-    </div>
-  )
+    return (
+        <div>
+            <Header/>
+            <MainContent/>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/mainContent/MainContent.tsx
@@ -0,0 +1,11 @@
+import {ContentContainer} from "./MainContent.styled.tsx";
+
+const MainContent = () => {
+    return (
+        <ContentContainer>
+            Main Content
+        </ContentContainer>
+    );
+};
+
+export default MainContent;
